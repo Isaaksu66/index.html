196e104f9526c8fdfedaff8d4ea6622e68e5d447
Isaaksu66 <isaaksu7777@gmail.com>
2025-04-16 22:29:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
+// Menu Burger Toggle pour mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
-  menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+// Lorsque l'utilisateur clique sur l'icône burger
+menuToggle.addEventListener('click', () => {
+  // Ajouter ou retirer la classe 'active' pour afficher/masquer le menu
+  navLinks.classList.toggle('active');
+});
+
+// Fonction pour fermer le menu lorsque l'un des liens est cliqué
+const navLinksItems = document.querySelectorAll('.nav-links a');
+
+navLinksItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Enlever la classe 'active' pour masquer le menu après un clic
+    navLinks.classList.remove('active');
   });
 });
